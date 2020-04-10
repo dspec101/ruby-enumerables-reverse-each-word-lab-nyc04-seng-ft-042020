3748acb4006830c34e16451dee33819e8f467573
David Spector <davidspector08@gmail.com>
2020-04-10 22:43:14 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+require 'pry'
+def reverse_each_word (string)
+    array = string.split (" ")
+    array2 = []
+      array.each do |string|
+      array2 << string.reverse
+     end
+       array2.join(" ")
+end
+     
+def reverse_each_word (string)
+   array = string.split
+   array.collect do |string|
+     string.reverse
+   end
+   array.join(" ")
+end
+  
