9b6b7b8c83eada6bfb9e6a679190e180e9783abb
David Spector <davidspector08@gmail.com>
2020-04-10 23:00:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word (string)
 end
      
 def reverse_each_word (string)
-   array = string.split
+   array = string.split (" ")
    array.collect do |string|
      string.reverse 
    end
